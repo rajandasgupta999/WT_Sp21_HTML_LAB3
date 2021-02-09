b51090682283b06be2e453a201862b990822e123
rajandasgupta999 <rajon168075@gmail.com>
2021-02-09 17:37:49 +0600

    Folder Created

---
+++ Problem3.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <title>problem1</title>
+  </head>
+  <body>
+    <?php
+    $a = 8;
+    $b = 6;
+    $area = $a*$b;
+    $parameter = 2*($a*$b);
+
+
+    if ($a == $b)
+     {
+        echo "the shape is a square";
+     }
+      ?>
+
+      <br>
+
+    <?php
+     echo "Area: $area";
+     ?>
+<br>
+
+<?php
+ echo "Parameter:  $parameter";
+ ?>
+
+  </body>
+<footer>
+<>
+</html>
---
+++ Problem4.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>problem1</title>
+  </head>
+  <body>
+    <?php
+    $name = "Pritha";
+    if($name == "raju" || $name == "Pritha" || $name == "Mithu")
+     {
+        echo "<b>hello</b>";
+     }
+
+
+    else echo "you are not recognized";
+
+     ?>
+
+  </body>
+</html>
